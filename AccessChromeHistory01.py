50590e00d8d7e23ec0dfe2c5ee61f55ec7805818
Satoshi Endo <zzz@65536.net>
2025-05-31 09:51:48 +0900

    Update AccessChromeHistory01.py

--- AccessChromeHistory01.py
+++ AccessChromeHistory01.py
@@ -8,7 +8,7 @@
 #2）Pythonの実行環境をあらかじめ用意してください。
 #    またコードを参照して必要なライブラリをインストールしてください。
 #3）以下のようにして実行。
-#    > python AccessChromeHistory01.py {URL_LIST_CSVで指定したファイル名}
+#    > python AccessChromeHistory01.py
 #4）ヘッドレスでの実行。
 #    HEADLESSにWebを自動的に開きながら（False）、Webは開かず（True）。
 #5）自動ログインしたいサイトがある場合はcookie情報を与える。
